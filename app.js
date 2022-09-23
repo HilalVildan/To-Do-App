cf4ce66b98e1a7774703fd16725e8d2959efea58
HilalVildan <hilalsarioglu96@gmail.com>
2022-09-23 15:20:15 +0200

    update

--- app.js
+++ app.js
@@ -7,109 +7,79 @@ const yapildi = document.querySelector("#tamamlanan");
 let liste = [];
 let index = 0;
 let total = 0;
+let clicked = false;
 let completed = 0;
 const todoList = document.querySelector("#todo-ul");
 
-
-
-
-
-ekleButon.addEventListener("click", () =>{
-
-    listeGöster();
-    girdi.value="";
-    
-})
-
+ekleButon.addEventListener("click", () => {
+  listeGöster();
+  girdi.value = "";
+});
 
 const listeGöster = () => {
+  liste.push([girdi.value, index, clicked]);
 
-liste.push([girdi.value, index])
-    
+  todoList.innerHTML = ``;
 
-
-todoList.innerHTML = ``
-
-    liste.forEach(element => {
-
-        todoList.innerHTML += `
-        <li class ="li-liste">
-        <a><i value=${element[1]} class="fa-solid fa-check"></i></a>
+  liste.forEach((element) => {
+    todoList.innerHTML += `
+        <li>
+        <i value=${element[1]} class="fa-solid fa-check"></i>
         <span class="yazi" value=${element[1]} >${element[0]}</span> 
-        <a><i value=${element[1]} class="fa-solid fa-trash"></i></a>
+        <i value=${element[1]} class="fa-solid fa-trash"></i>
         </li>`;
 
-            const check = document.querySelectorAll(".fa-check").forEach(elementCheck => {
+    const check = document
+      .querySelectorAll(".fa-check")
+      .forEach((elementCheck) => {
+        elementCheck.onclick = () => {
+          let valueCheck = elementCheck.getAttribute("value");
+          // console.log(liste[valueCheck]);
+          elementCheck.style.color = "green";
 
-                elementCheck.onclick = () => {
+          document.querySelectorAll(".yazi")[valueCheck].style.textDecoration =
+            "line-through";
 
-                    let valueCheck = elementCheck.getAttribute("value");
-                    // console.log(liste[valueCheck]);
-                    elementCheck.style.color = "green"
-                    
-                    document.querySelectorAll(".yazi")[valueCheck].style.textDecoration =
-                      "line-through";
+          // console.log(document.querySelectorAll(".yazi")[valueCheck]);
 
-                      // console.log(document.querySelectorAll(".yazi")[valueCheck]);
+          completed += 1;
+          yapildi.textContent = completed;
+        };
+      });
+    const sil = document.querySelectorAll(".fa-trash").forEach((elementSil) => {
+      elementSil.onclick = () => {
+        let valueSil = elementSil.getAttribute("value");
 
-                       completed +=1
-                      yapildi.textContent = completed
+        for (let index = 0; index < sil.length; index++) {
+          if (liste[index][1] == valueSil) {
+            liste.splice(index, 1);
+            console.log(document.querySelectorAll("li"));
 
+            document.querySelectorAll("li")[valueSil].innerHTML = ``;
 
-                    
-                    
+            console.log(liste, "liste");
+            console.log(valueSil);
+            
+          }
+        }
 
-                }
-               
-            });
-        
-            const sil = document.querySelectorAll(".fa-trash").forEach(elementSil => {
+        // const liList = document.querySelectorAll(".li-liste");
 
+        //console.log(liste,valueSil);
 
+        total -= 1;
+        toplam.textContent = total;
 
-                elementSil.onclick = () => {
-
-                    let valueSil = elementSil.getAttribute("value");
-
-                const silinen =liste.filter(item => !valueSil.includes(item))
-                console.log(silinen);
-                  // liste.splice(valueSil,0)
-
-
-
-                    // const liList = document.querySelectorAll(".li-liste");
-                    
-                    document.querySelectorAll(".li-liste")[valueSil].innerHTML = ``; 
-                   //console.log(liste,valueSil);
-                    
-                    
-
-                    total -= 1;
-                    toplam.textContent = total;
-
-                    if (completed > 1){
-                        completed -=1;
-                        yapildi.textContent = completed;
-                    };
-                    
-
-
-                    
-                }
-            })
-
-
-
-
+       if (completed > 1) {
+          completed -= 1;
+          yapildi.textContent = completed; 
+        }
+      };
     });
-total += 1;
-toplam.textContent = total
+  });
+  total += 1;
+  toplam.textContent = total;
 
-index +=1;
-console.log(liste);
-
-
-
-}
-
-
+  index += 1;
+  console.log(liste);
+};
